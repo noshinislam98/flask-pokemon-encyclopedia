7967d1c3cb56c2ad6771db0fbf92ba6ddf218f2e
Noshin Islam <noshinislam98@gmail.com>
2022-10-17 15:33:28 +0100

    updated pokemon data endpoints into a single function

--- app.py
+++ app.py
@@ -4,35 +4,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
+pokemon_creatures = {
+    "bulbasaur": "dinosaur",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole"
+}
+
+
 @app.get("/")
 def pokemon_list():
     return "Bulbasaur, charmander, pikachu, eevee, diglett"
 
 
-@app.get("/bulbasaur")
-def bulbasaur_data():
-    return "This is Bulbasaur, a generation 1 pokemon who looks like a tiny dinosaur."
-
-
-@app.get("/charmander")
-def charmander_data():
-    return "This is Charmander, a generation 1 pokemon who looks like a tiny reptile."
-
-
-@app.get("/pikachu")
-def pikachu_data():
-    return "This is Pikachu, a generation 1 pokemon who looks like a tiny rodent."
-
-
-@app.get("/eevee")
-def eevee_data():
-    return "This is Eevee, a generation 1 pokemon who looks like a tiny fox."
-
-
-@app.get("/diglett")
-def diglett_data():
-    return "This is Diglett, a generation 1 pokemon who looks like a tiny mole."
+@app.get("/<pokemon_name>")
+def pokemon_data(pokemon_name):
+    creature = pokemon_creatures.get(pokemon_name)
+    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}."
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)
